1305128345a794889824bdc05f72a971412ebfcf
zacharylin <zacharylin123@gmail.com>
2019-06-15 16:56:25 +1000

    add connect

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { statement } from '@babel/template';
 
 class SongList extends React.Component {
+    renderList(){
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            )
+        })
+    }
+
+
     render() {
-        return <div>SongList</div>
+        // this.props === { songs: state.songs}
+        console.log(this.props);
+        return <div className="ui divided list">{this.renderList()}</div>
     }
 }
 
-export default connect()(SongList);
+
+const mapStateToProps = (state) => {
+    // console.log(state);
+    // to get the songs from provider
+    return { songs: state.songs};
+}
+
+export default connect(mapStateToProps)(SongList);
